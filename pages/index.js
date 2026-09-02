e20334f26bd9acf700f05f316e25b3a0bfae6d92
leticiadelchello <leticia.delchello@gmail.com>
2026-09-02 22:56:34 +0000

    Alteração da frase do site

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Vamos fingir que sabado não passou ainda e que o projeto dos Scripts em Python pode ser entregue mais para frente.</h1>
+    return <h1>Vamos fingir que sabado não passou e que o projeto dos Scripts em Python é para ser entregue mais para frente.</h1>
 }
 
 export default Home;
