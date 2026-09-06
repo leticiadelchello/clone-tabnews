491a1f2100ecf1a8f0e5540d37e586c35992a1b5
leticiadelchello <leticia.delchello@gmail.com>
2026-09-06 22:34:18 +0000

    atualizações de estudos da semana

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Vamos fingir que sabado não passou e que o projeto dos Scripts em Python é para ser entregue mais para frente.</h1>
+    return <p>Atualizações: vi a aula 1 de duas matérias muito blé (formação EAD e formação cidadã contemporânea), assisti uma 2 aulas de matemática elementar, assisti 2 aulas de introdução a computação. Fiz cartas no anki para praticar, fiz alguns exercícios de matemática da matérias, li o texto da aula 1 de introdução à computação e comecei a ler o da segunda aula. Continua...</p>
 }
 
 export default Home;
